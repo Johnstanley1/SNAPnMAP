3137613755e9ae31ecdfb71c920895f27ec82358
rinzl <wrs0251@gmail.com>
2024-04-06 10:52:50 -0400

    Updated Card-Display, Added 'Sort Button'
    Changed how the photos generate on the photo page, making it look more like an album.
    
    Also added a 'Sort' button that changes it's value when clicked (Functionality NOT INCLUDED)

--- src/app/photopage/photopage.component.ts
+++ src/app/photopage/photopage.component.ts
@@ -11,5 +11,24 @@ import {RouterLink} from "@angular/router";
   styleUrl: './photopage.component.css'
 })
 export class PhotopageComponent {
+  sortData: string[] = ["A - Z", "Added", "Rating", "Favourite"];
+  sortInt: number = 0;
+  sortString : string = this.sortData[1]
+  constructor() {
 
+  }
+
+  changeSortValue_click(){
+    // Increment sortInt and adjust sortString data:
+    this.sortInt++;
+    this.sortString = this.sortData[this.sortInt]
+
+    // Check if sortInt is out of bounds:
+    if(this.sortInt >= this.sortData.length){
+      this.sortInt = 0;
+      this.sortString = this.sortData[this.sortInt]
+    }
+
+    // TODO: ADJUST THE CARDS ACCORDINGLY:
+  }
 }
