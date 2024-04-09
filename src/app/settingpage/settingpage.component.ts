2bdebf3f7d3bb989d08c4e792db180696a28c052
rinzl <wrs0251@gmail.com>
2024-04-09 15:29:31 -0400

    Password adding/updating on settings

--- src/app/settingpage/settingpage.component.ts
+++ src/app/settingpage/settingpage.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import {RouterLink, RouterLinkActive} from "@angular/router";
 import {DALService} from "../../../services/DAL-service";
@@ -17,58 +17,112 @@ import {DALService} from "../../../services/DAL-service";
 
 export class SettingpageComponent {
 
+  // Password param:
+  passwordSet: string = "Hidden Password Not Set";
   // Inject DAL:
   dal_service = inject(DALService)
 
+  ngOnInit(): void {
+    if (localStorage.getItem("Password") != null) {
+      this.passwordSet = "Password Set";
+    } else {
+      this.passwordSet = "Password NOT Set";
+    }
+  }
+
   // Database functions:
-  clearPhotos_click(){
-    if(confirm("Are you Sure you want to Delete all Photos? 💔")){
+  clearPhotos_click() {
+    if (confirm("Are you Sure you want to Delete all Photos? 💔")) {
       this.dal_service
         .deleteAllPhotos()
-        .then( () => {
+        .then(() => {
           alert("All Photos Deleted Successfully.😢")
         })
-        .catch( (e) => {
+        .catch((e) => {
           alert("Failed to Delete All Photos: " + e.message)
         });
-    }else{
+    } else {
       alert("Didnt think so 😉.");
     }
   }
 
-  clearCollections_click(){
-    if(confirm("Are you Sure you want to Delete all Collections? 💔")){
+  clearCollections_click() {
+    if (confirm("Are you Sure you want to Delete all Collections? 💔")) {
       this.dal_service
         .deleteAllPhotos()
-        .then( () => {
+        .then(() => {
           alert("All Collections Deleted Successfully.😢")
         })
-        .catch( (e) => {
+        .catch((e) => {
           alert("Failed to Delete All Collections: " + e.message)
         });
-    }else{
+    } else {
       alert("Didnt think so 😉.");
     }
   }
 
-  clearDatabase_click(){
-    if(confirm("Are you Sure you want to Clear the Database? 💔")){
+  clearDatabase_click() {
+    if (confirm("Are you Sure you want to Clear the Database? 💔")) {
       this.dal_service
         .deleteAllPhotos()
-        .then( () => {
+        .then(() => {
           alert("Database Cleared Successfully.😢")
         })
-        .catch( (e) => {
+        .catch((e) => {
           alert("Failed to Clear Database: " + e.message)
         });
-    }else{
+    } else {
       alert("Didnt think so 😉.");
     }
   }
 
-  setPassword_click(){
-    prompt("Please Provide a Password:");
+  setPassword_click() {
+    // Check localstorage for a password:
+    if (localStorage.getItem("Password") != null) {
+      // If yes, prompt for reset:
+      if (confirm("There is already a saved password, reset?")) {
+        // Request old password:
+        const oldPassword: any = prompt("Please Enter your OLD Password:");
+
+        // Check if the password matches:
+        if (oldPassword === localStorage.getItem("Password")) {
+
+          // If they match, get new password:
+          const password: any = prompt("Please Provide a Password:");
+          const confirmPass: any = prompt("Please Confirm Your Password:");
+
+          // Check if passwords match
+          if (password === confirmPass) {
+            localStorage.setItem("Password", password);
+            this.passwordSet = "Password Set"
+          }
+          else{
+            alert("Passwords don't match.");
+          }
+        }
+        else{
+          alert("Incorrect Password.");
+        }
+      }
+    }
+    else{
+
+      // get new password
+      const password: any = prompt("Please Provide a New Password:");
+      const confirmPass: any = prompt("Please Confirm Your New Password:");
+
+      // Check if match
+      if (password === confirmPass) {
+
+        // update password in localstorage
+        localStorage.setItem("Password", password);
+
+        // Update button:
+        this.passwordSet = "Password Set"
+      }
+      else {
+        alert("Passwords don't Match. Please Try Again.");
+      }
+    }
   }
-
-
 }
