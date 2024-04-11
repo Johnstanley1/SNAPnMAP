b1a19631a0e509c77349d0e5bd3168f15dfa5679
rinzl <wrs0251@gmail.com>
2024-04-11 12:02:13 -0400

    Updated CollectionPage Functionality to Match PhotoPage

--- src/app/collectionspage/collectionspage.component.ts
+++ src/app/collectionspage/collectionspage.component.ts
@@ -1,15 +1,41 @@
-import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import { Component, inject, OnInit } from '@angular/core';
+import {RouterLink, Router} from "@angular/router";
+import {DALService} from "../../../services/DAL-service";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-collectionspage',
   standalone: true,
     imports: [
-        RouterLink
+      RouterLink,
+      NgForOf,
+      NgIf
     ],
   templateUrl: './collectionspage.component.html',
   styleUrl: './collectionspage.component.css'
 })
 export class CollectionspageComponent {
 
+  // Initialize empty collections array:
+  collections: any[] = [];
+
+  // Inject DAL Service:
+  loadCollection = inject(DALService);
+
+  // Constructor
+  constructor(private router: Router) { }
+
+  ngOnInit(){
+    // Load collections from Db:
+    this.loadCollections();
+  }
+
+  loadCollections(){
+    this.loadCollection
+      .selectAllCollections()
+      .then( (data) => {
+        // Initialize collections with data received:
+        this.collections = data;
+      })
+  }
 }
